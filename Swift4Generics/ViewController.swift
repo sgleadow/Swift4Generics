34bb15224be4f84b3b70a0159219d96c15b8b8d6
Stewart Gleadow <sgleadow@gmail.com>
2017-08-10 07:57:56 +1000

    initial commit

---
+++ Swift4Generics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4Generics
+//
+//  Created by Stewart Gleadow on 10/8/17.
+//  Copyright © 2017 Stewart Gleadow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
